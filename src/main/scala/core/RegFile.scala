665d38869d905be9ade3da05a679726a25abac17
Zachary Yedidia <zyedidia@gmail.com>
2022-03-11 22:54:27 -0500

    Fix regfile

--- src/main/scala/core/RegFile.scala
+++ src/main/scala/core/RegFile.scala
@@ -20,8 +20,8 @@ class RegFile(size: Int, width: Int) extends Module {
 
   val regs = Mem(size, UInt(width.W))
 
-  io.rdata0 := Mux(io.raddr0 === 0.U(width.W), 0.U(width.W), regs(io.raddr0))
   io.rdata1 := Mux(io.raddr1 === 0.U(width.W), 0.U(width.W), regs(io.raddr1))
+  io.rdata2 := Mux(io.raddr2 === 0.U(width.W), 0.U(width.W), regs(io.raddr2))
 
   when (io.wen && io.waddr =/= 0.U(width.W)) {
     regs(io.waddr) := io.wdata
