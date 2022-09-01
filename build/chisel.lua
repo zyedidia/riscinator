203429bfc0d57007d15d3c0aa6ab5e2c818650da
Zachary Yedidia <zyedidia@gmail.com>
2022-08-31 18:28:28 -0700

    Use directory rules

--- build/chisel.lua
+++ build/chisel.lua
@@ -4,22 +4,25 @@ function chisel.build(src, pkg, top, gen)
     local firflags := --disable-annotation-unknown -O=release --lowering-options=noAlwaysComb,disallowPackedArrays,disallowLocalVariables
     local sbt = "sbt --client"
     return {
-    $ $gen/%.fir: $src serve
+    $ $gen/%: $gen/%.fir
+        firtool --split-verilog -o $gen/$match $input --annotation-file=$gen/$match.anno.json $firflags
+
+    $ $gen/%.fir: $src serve gen
         mkdir -p $gen
         $sbt runMain $pkg.$match
 
-    $ $gen/Soc.fir: $src serve
+    $ $gen/Soc.fir: $src serve gen
         mkdir -p $gen
         $sbt runMain $pkg.Soc
 
     $ $gen/%.sv: $gen/%.fir
         firtool --verilog -o $output $input --annotation-file=$gen/$match.anno.json $firflags
 
-    $ $gen/(.*)/(.*).sv:R: $gen/$$1.fir
-        firtool --split-verilog -o $gen/$match1 $input --annotation-file=$gen/$match1.anno.json $firflags
-
     $ serve:VL:
         $sbt exit
+
+    $ gen:VL:
+        mkdir -p $gen
 
     $ format:VB:
         $sbt scalafmtAll
