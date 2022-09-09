f902a68b9d71faeb23bd97555bbf658151bb9611
Zachary Yedidia <zyedidia@gmail.com>
2022-09-08 17:12:46 -0700

    Symbolic simulation

--- tests/tb-arc.cc
+++ tests/tb-arc.cc
@@ -41,13 +41,11 @@ static unsigned addr2idx(uint32_t addr, size_t mem_base) {
     return (addr-mem_base) / sizeof(uint32_t);
 }
 
-static bool failed = false;
-
-static void simulate(CoreView core, void* statePtr, uint32_t* mem, size_t len, size_t mem_base, check_t* check) {
+static void simulate(Core &core, uint32_t* mem, size_t len, size_t mem_base) {
     core.reset = 1;
-    Core_passthrough(statePtr);
-    Core_clock(statePtr);
-    Core_passthrough(statePtr);
+    core.passthrough();
+    core.clock();
+    core.passthrough();
     core.reset = 0;
 
     uint32_t next_imem_rdata = 0;
@@ -55,9 +53,9 @@ static void simulate(CoreView core, void* statePtr, uint32_t* mem, size_t len, s
     uint32_t next_dmem_rdata = 0;
     uint8_t next_dmem_rvalid = 0;
 
-    unsigned ncyc = 50;
+    unsigned ncyc = 4;
     for (unsigned i = 0; i < ncyc; i++) {
-        Core_passthrough(statePtr);
+        core.passthrough();
 
         core.io_imem_rvalid = next_imem_rvalid;
         core.io_imem_rdata = next_imem_rdata;
@@ -78,30 +76,12 @@ static void simulate(CoreView core, void* statePtr, uint32_t* mem, size_t len, s
             uint32_t mask = be2mask(core.io_dmem_be);
             assert(addr2idx(core.io_dmem_addr, mem_base) < len);
             mem[addr2idx(core.io_dmem_addr, mem_base)] = write & mask;
-            printf("%x: dmem write: %d\n", core.io_dmem_addr, write & mask);
         } else if (core.io_dmem_req) {
             assert(addr2idx(core.io_dmem_addr, mem_base) < len);
             next_dmem_rdata = mem[addr2idx(core.io_dmem_addr, mem_base)];
         }
 
-        Core_clock(statePtr);
-    }
-
-    for (auto v : check->regs) {
-        auto got = core.internal.rf.regs_ext.words[v.idx].data;
-        auto expected = v.value;
-        if (got != expected) {
-            printf("FAIL: regs[%d]: %d != %d\n", v.idx, got, expected);
-            failed = true;
-        }
-    }
-    for (auto v : check->mem) {
-        auto got = mem[addr2idx(v.idx, mem_base)];
-        auto expected = v.value;
-        if (got != expected) {
-            printf("FAIL: mem[%x]: %d != %d\n", v.idx, got, expected);
-            failed = true;
-        }
+        core.clock();
     }
 }
 
@@ -109,76 +89,20 @@ static void simulate(CoreView core, void* statePtr, uint32_t* mem, size_t len, s
 #define MEMBASE 0x100000
 static uint32_t mem[MEMSIZE];
 
-static void itype() {
-    std::vector<uint8_t> state(CoreLayout::numStateBytes, 0);
-    CoreView core(&state[0]);
-    void* statePtr = static_cast<void*>(&state[0]);
-
-    memset(mem, 0, sizeof(mem));
-    memcpy(mem, itype_bin, itype_bin_len);
-    check_t* check = (check_t*) calloc(sizeof(check_t), 1);
-    check->regs = {
-        {1, 63},
-        {2, 3},
-        {3, 67},
-        {4, 3},
-        {5, (uint32_t)-1},
-        {7, 1},
-        {8, 0x100004},
-        {9, 63},
-        {10, 3},
-        {11, 67},
-    };
-    check->mem = {
-        {0x100000, 3},
-        {0x100004, 63},
-        {0x100008, 67},
-    };
-    simulate(core, statePtr, (uint32_t*) itype_bin, MEMSIZE, MEMBASE, check);
-}
-
-static void jmps() {
-    std::vector<uint8_t> state(CoreLayout::numStateBytes, 0);
-    CoreView core(&state[0]);
-    void* statePtr = static_cast<void*>(&state[0]);
-
-    memset(mem, 0, sizeof(mem));
-    memcpy(mem, jmps_bin, jmps_bin_len);
-    check_t* check = (check_t*) calloc(sizeof(check_t), 1);
-    check->regs = {
-        {1, 0},
-        {2, 3},
-        {3, 67},
-        {4, 3},
-        {5, (uint32_t)-1},
-        {7, 1},
-        {8, 0x100004},
-        {9, 63},
-        {10, 3},
-        {11, 67},
-        {13, 1048688},
-        {17, 1},
-        {19, 1},
-        {20, 1},
-        {21, 1},
-        {22, 1},
-        {23, 1},
-        {25, 1},
-        {26, 63},
-    };
-    check->mem = {
-        {0x100000, 3},
-        {0x100004, 63},
-        {0x100008, 67},
-    };
-    simulate(core, statePtr, (uint32_t*) jmps_bin, MEMSIZE, MEMBASE, check);
-}
-
 int main(int argc, char **argv) {
-    itype();
-    jmps();
+    Core core;
 
-    if (failed) return 1;
-    printf("ALL TESTS PASSED\n");
+    memset(mem, 0, sizeof(mem));
+    uint32_t insn;
+    rvsym_mark_bytes(&insn, sizeof(insn), "insn");
+    // rvsym_assume(insn == 0x02a00093);
+    rvsym_assume((insn & 0b1111111) == 0b0010011);
+    mem[0] = insn;
+    mem[1] = 0x0000006f;
+    simulate(core, mem, MEMSIZE, MEMBASE);
+    if (core.internal.rf.regs_ext.words[1].data == 42) {
+        rvsym_exit();
+    }
+
     return 0;
 }
