657efb86d5bd812d1b01154de6ba12dcb660c8a5
Zachary Yedidia <zyedidia@gmail.com>
2022-09-28 16:02:33 -0700

    Add conf opts

--- build/ulx3s.lua
+++ build/ulx3s.lua
@@ -7,7 +7,7 @@ function ulx3s.build(cons, wrap, top, verilog, dep, quiet)
     $ synth:V: $top.bit
 
     $ prog:V: $top.bit
-        fujprog $input
+        sudo fujprog $input
 
     $ $top.json: $wrap $dep
         yosys $q -p 'read_verilog -sv -DTOP=$top -noautowire $verilog $wrap; hierarchy -top top; synth_ecp5 -top top -json $output'
---
+++ conf.lua
@@ -0,0 +1,6 @@
+return {
+    prog = cli.prog or "blink",
+    quiet = cli.quiet or true,
+    top = cli.top or "Soc",
+    tech = cli.tech or "ulx3s",
+}
