a0c1fb6a3624b9f8b2051f2f927997db6d54c62c
Zachary Yedidia <zyedidia@gmail.com>
2022-09-30 16:09:59 -0700

    Fix build issue

--- build/verilator.lua
+++ build/verilator.lua
@@ -3,7 +3,7 @@ local verilator = {}
 function verilator.tb(tb, verilog, top, dep)
     local vdir = ".verilator"
     return {
-    $ $vdir/tb: $tb $dep generated/$top
+    $ $vdir/tb: $tb $dep
         verilator --public -sv -cc -Mdir $vdir $verilog --top $top --exe --build $tb -o tb
     $ test:VB: $vdir/tb
         ./$input
@@ -15,7 +15,7 @@ function verilator.sim(sim, verilog, top, dep)
     return {
     $ tests/rtor_mem.h: tests/rtor.bin
         cd tests; xxd --include rtor.bin > rtor_mem.h
-    $ $vdir/sim: $sim $dep generated/$top
+    $ $vdir/sim: $sim $dep
         verilator --public -sv -cc -Mdir $vdir $verilog --top $top --exe --trace --build $sim -o sim
     $ sim:VB: $vdir/sim
         ./$input
